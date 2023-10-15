2a15a545a0cb2f9fe10c5d70ccce05a28724f4e0
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2023-10-15 09:04:54 -0600

    Update getData.js

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,39 +1,38 @@
-const needle = require('needle');
-
-exports.handler = async (event, context) => {
-  const { FIREBASE_DB_URL } = process.env;
-
-  if (!FIREBASE_DB_URL) {
-    return {
-      statusCode: 500,
-      body: 'Firebase Realtime Database URL is not configured.',
-    };
-  }
-
-  try {
-    const response = await needle('get', `${FIREBASE_DB_URL}.json`);
-
-    if (response.statusCode !== 200) {
-      throw new Error('Failed to retrieve data from Firebase.');
+exports.handler = async (event) => {
+    const { FIREBASE_DB_URL } = process.env;
+  
+    if (!FIREBASE_DB_URL) {
+      return {
+        statusCode: 500,
+        body: 'Firebase Realtime Database URL is not configured.',
+      };
     }
-
-    const data = response.body;
-
-    // Create a response object with custom headers
-    const headers = {
-        "Access-Control-Allow-Origin": "https://emailauth-e6005.web.app",
-        "Cache-Control": "public, max-age=180" // Set max-age to 180 seconds (3 minutes)
-    };
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-      headers, // Include custom headers in the response
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: `Error: ${error.message}`,
-    };
-  }
-};
+  
+    try {
+      const response = await fetch(`${FIREBASE_DB_URL}.json`); // Use fetch for edge functions
+  
+      if (response.ok) {
+        const data = await response.json();
+  
+        // Create a response object with custom headers
+        const headers = {
+          "Access-Control-Allow-Origin": "https://emailauth-e6005.web.app",
+          "Cache-Control": "public, max-age=180" // Set max-age to 180 seconds (3 minutes)
+        };
+  
+        return {
+          statusCode: 200,
+          body: JSON.stringify(data),
+          headers, // Include custom headers in the response
+        };
+      } else {
+        throw new Error('Failed to retrieve data from Firebase.');
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: `Error: ${error.message}`,
+      };
+    }
+  };
+  
